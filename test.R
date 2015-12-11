cf3b01e8cc72fba1c50d2db09e2fc104068b8b19
hh1985 <hh.uslife@gmail.com>
2015-12-10 21:33:09 -0500

    Demo case using xgboost

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 if (F) {
   sample.submission <- read.csv("sample_submission.csv", sep = "\t")
   store <- read.csv("store.csv")
-  test <- read.csv("test.csv", row.names = 1)
+  final.test <- read.csv("test.csv", row.names = 1)
   train <- read.csv("train.csv")
   str(sample.submission)
   str(store)
@@ -39,6 +39,8 @@ if (F) {
 # 1a. weekday/weekend
 # 1b. proceeding day
 # 1c. Promotion days.
+train.date <- data.frame()
+
 # 2. Store: store type, assortment, competition
 # 3. Promotion.
 # 3a. If correlation with Holiday.
@@ -46,4 +48,46 @@ if (F) {
 # 5. Customers: removed in test dataset.
 if (T) {
    
+}
+
+# XGBoost test case.
+require(xgboost)
+require(Matrix)
+require(data.table)
+if (F) {
+  train.df <- data.table(train)
+  # The store id is still important.
+  train.df$Store <- factor(paste("ST", train.df$Store, sep = ""))
+  store.df <- data.table(store)
+  store.df$Store <- factor(paste("ST", store.df$Store, sep = ""))
+  final.df <- data.table(final.test)
+  final.df$Store <- factor(paste("ST", final.df$Store, sep = ""))
+  
+  require(dplyr)
+  expand.df <- left_join(train.df, store.df, by = "Store")
+
+  # Create dummy variable
+  library(caret)
+  # Customer can be used to weight Store.
+  #dmy <- dummyVars(Sales~. - Date - Store - Customers, data = expand.df)
+  dmy <- dummyVars(Customers~. - Date - Store - Sales, data = expand.df)
+  expand.train <- predict(dmy, newdata = expand.df)
+  #sparse.train <- sparse.model.matrix(Sales ~. - Date - Store, data = expand.df)
+  
+  # Each key is (Store, Date) and the sales should meet: sales = f(Store, Date, Competitor)
+  #train.Y <- train.df[,  Sales]
+  train.Y <- train.df[,  Customers]
+  bst <- xgb.cv(data = expand.train,
+                 label = train.Y,
+                 max.depth = 8,
+                 eta = 1,
+                 nfold = 5,
+                 nthread = 2,
+                 nround = 5000,
+                 objective = "reg:linear",
+                 maximize = FALSE,
+                 early.stop.round = 1000
+                 )
+  importance <- xgb.importance(feature_names = dimnames(expand.train)[[2]], model = bst)
+  head(importance)
 }
